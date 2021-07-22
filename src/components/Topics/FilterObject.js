9470af84fc0230deef695d857d144bedaae0b82e
carterhanks <carterhanks@gmail.com>
2021-07-21 20:17:27 -0600

    Initial Commit, Partailly Finished

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 export default class FilterObject extends Component {
+    constructor() {
+        super();
+        this.state = {
+            
+        }
+    }
+
+
     render() {
         return(
             <div className="puzzleBox filterObjectPB">
